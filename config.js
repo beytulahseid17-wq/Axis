f0f3d53da3bfe8415dcfad6a0b125900b5b92ad9
Beytulah <beytulahseid17@gmail.com>
2026-07-14 04:44:12 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // These are safe to expose in frontend code — they only allow what your
 // Row Level Security policies in schema.sql permit (each user can only see their own data).
 
-var SUPABASE_URL = "PASTE_YOUR_PROJECT_URL_HERE";
-var SUPABASE_ANON_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+var SUPABASE_URL = "https://jcfqjltjnkocjmctnsth.supabase.co/rest/v1/";
+var SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjZnFqbHRqbmtvY2ptY3Ruc3RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwMTUwNTEsImV4cCI6MjA5OTU5MTA1MX0.t2U8GsWpm8J3HMj6nmFIwv5RA2dhaRrLo8YdcMnVP7M";
