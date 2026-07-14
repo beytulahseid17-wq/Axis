2b6400a1be28f4d9136723be5ddef78fe4902ffa
Beytulah <beytulahseid17@gmail.com>
2026-07-14 04:50:43 -0700

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -6,4 +6,4 @@
 // Row Level Security policies in schema.sql permit (each user can only see their own data).
 
 var SUPABASE_URL = "https://jcfqjltjnkocjmctnsth.supabase.co/rest/v1/";
-var SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjZnFqbHRqbmtvY2ptY3Ruc3RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwMTUwNTEsImV4cCI6MjA5OTU5MTA1MX0.t2U8GsWpm8J3HMj6nmFIwv5RA2dhaRrLo8YdcMnVP7M";
+var SUPABASE_ANON_KEY = "sb_publishable_jpwzeaA48qFcOIQEVu1zSg_mEv2Br-8";
